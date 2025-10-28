83b6a497b78e545f4f846a1844a19a73f4847952
gr3yai2thur <bovonnatchaladdee@gmail.com>
2025-10-28 21:47:43 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Main
+{
+    public static void main(String[] args)
+    {
+        int width, lenght;
+        Scanner In = new Scanner(System.in);
+        System.out.print("Input Width: ");
+        width = In.nextInt();
+
+        System.out.print("Input Lenght: ");
+        lenght = In.nextInt();
+
+        int area = width*lenght;
+
+        System.out.println("Area = " + area);
+    }
+}
