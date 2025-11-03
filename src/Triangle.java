e071ceb56314a4daeb4659dd0c899168d87361b9
S i r H u s k y <bovonnatchaladdee@gmail.com>
2025-11-03 21:10:47 +0700

    Update Triangle.java

--- src/Triangle.java
+++ src/Triangle.java
@@ -2,7 +2,7 @@ import java.lang.classfile.attribute.SyntheticAttribute;
 import java.util.Scanner;
 
 public class Triangle {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         int height, lenght;
         Scanner In = new Scanner(System.in);
         System.out.print("Enter Height: ");
